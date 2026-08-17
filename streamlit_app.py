369a9c6ec4ab53c234b5bce31df733aea3a083bc
Triveni9481 <triveni11042005@gmail.com>
2026-08-17 23:52:04 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,7 +18,8 @@ st.write(
 )
 
 # Get active Snowflake session
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Get fruit options
 my_dataframe = session.table(
